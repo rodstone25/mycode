0a4239c6ea40cfc02c105215bee4981b04abe200
rodstone25 <anderro25@gmail.com>
2018-10-22 20:18:14 -0400

    added commects to program

---
+++ lab_input/input_ip.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Written by Rodney Anderson
+
+# Collect input from user
+user_input = input('Please an IPv4 address:')
+
+# print out IP Address collected from User
+print("You told me the IPv4 address is:" + user_input)
