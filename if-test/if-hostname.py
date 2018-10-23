948771107aff8f9f6b55480685758fc4747710dd
rodstone25 <anderro25@gmail.com>
2018-10-23 13:57:53 -0400

    esle-if Statement

---
+++ if-test/if-hostname.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+user_input = input('Please enter an in hostname:')
+
+#if hostname == user_input':
+print("You told me the Hostname:" + user_input)
+#print('The hostname was found to be mtg')
